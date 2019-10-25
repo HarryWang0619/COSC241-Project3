491250d3891fe92309ef900e3dd9e08dfd91c7e4
HarryWang0619 <harrywang0619@gmail.com>
2019-10-25 01:22:16 -0400

    Correct file uploaded.
    That was just a tutorial of RL.

--- reinforce-master/reinforce/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from . import core
-from . import gridworld
-from . import puckworld
-from . import approximator
-from . import agents
-
-__version__ = '0.0.1'
